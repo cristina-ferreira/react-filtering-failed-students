9dfcba216e64b58ca36791dd85e60f1997a5dfe2
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 15:57:28 +0100

    Updates

---
+++ precache-manifest.f464c3cc68fe68db088592be20e186fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1e66c6a824f8d27439081641a5950f",
+    "url": "/react-filtering-failed-students/index.html"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/react-filtering-failed-students/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "9f628422193fbab31ea8",
+    "url": "/react-filtering-failed-students/static/js/main.74fe88c4.chunk.js"
+  },
+  {
+    "revision": "7c71d70fd8aa0fabf2d1",
+    "url": "/react-filtering-failed-students/static/js/runtime~main.1b993c29.js"
+  }
+]);
